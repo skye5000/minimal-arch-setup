b4909c7612063763774cddd79730dc2885fcac84
skye5000 <skylykat69@gmail.com>
2021-12-28 18:58:10 +0200

    bye rust <3

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd aura-bin
 makepkg -i --noconfirm
 cd ~
 rm aura-bin -rf
-sudo aura -Syu rust xorg-server xorg-xinit xorg-xset unclutter bspwm git noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
+sudo aura -Syu xorg-server xorg-xinit xorg-xset unclutter bspwm git noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
 sudo aura -Aax xst-git siji-git --noconfirm
 git clone https://github.com/skye5000/minimal-arch-setup.git
 mv minimal-arch-setup/* .
