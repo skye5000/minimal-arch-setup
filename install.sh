01afd5909dcccbdcde29bab72afac67557eee5ba
skye5000 <skylykat69@gmail.com>
2021-12-28 18:52:10 +0200

    oopsie

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 cd ~
-sudo pacman -Syu rust xorg-server xorg-xinit xorg-xset unclutter bspwm git noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
-sudo aura -Aax xst-git siji-git --noconfirm
 git clone https://aur.archlinux.org/aura-bin.git
 cd aura-bin
 makepkg -i --noconfirm
 cd ~
 rm aura-bin -rf
+sudo aura -Syu rust xorg-server xorg-xinit xorg-xset unclutter bspwm git noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
+sudo aura -Aax xst-git siji-git --noconfirm
 git clone https://github.com/skye5000/minimal-arch-setup.git
 cd minimal-arch-setup
 mv minimal-arch-setup/* .
