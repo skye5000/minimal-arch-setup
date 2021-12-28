dd92b9bee8c102effcfc5d891542485e774e0ad2
skye5000 <skylykat69@gmail.com>
2021-12-28 18:55:39 +0200

    oops

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ rm aura-bin -rf
 sudo aura -Syu rust xorg-server xorg-xinit xorg-xset unclutter bspwm git noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
 sudo aura -Aax xst-git siji-git --noconfirm
 git clone https://github.com/skye5000/minimal-arch-setup.git
-cd minimal-arch-setup
 mv minimal-arch-setup/* .
 cd ~
 rm minimal-arch-setup -rf
 rm .terminfo -rf
+rm install.sh
 mkdir -p /etc/systemd/system/getty@tty1.service.d
 cat > /etc/systemd/system/getty@tty1.service.d/override.conf <<- "_EOF_"
 [Service]
