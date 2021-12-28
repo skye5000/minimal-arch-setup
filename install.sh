af38d3e0947e4dd6922863e28821840f6d9c7d3a
skye5000 <skylykat69@gmail.com>
2021-12-28 19:02:44 +0200

    :l

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 cd ~
+sudo pacman -S git --noconfirm
 git clone https://aur.archlinux.org/aura-bin.git
 cd aura-bin
 makepkg -i --noconfirm
 cd ~
 rm aura-bin -rf
-sudo aura -Syu xorg-server xorg-xinit xorg-xset unclutter bspwm git noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
+sudo aura -Syu xorg-server xorg-xinit xorg-xset unclutter bspwm noto-fonts noto-fonts-cjk noto-fonts-emoji xfce-fonts-misc --noconfirm
 sudo aura -Aax xst-git siji-git --noconfirm
 git clone https://github.com/skye5000/minimal-arch-setup.git
 mv minimal-arch-setup/* .
