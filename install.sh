005033e8856dbbe6fe08f726a7b14b168025e159
skye5000 <skylykat69@gmail.com>
2021-12-28 20:08:24 +0200

    p

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ makepkg -i --noconfirm
 cd ~
 rm aura-bin -rf
 sudo aura -Syu xorg-server xorg-xinit xorg-xset unclutter bspwm noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm
-sudo aura -Aax xst-git siji-git polybar --noconfirm
+sudo aura -Aax xst-git siji-git --noconfirm
 git clone https://github.com/skye5000/minimal-arch-setup.git
 mv minimal-arch-setup/* ~
 cd ~
@@ -17,4 +17,5 @@ rm install.sh
 rm README.md
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 sudo mv override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo aura -Aax polybar --noconfirm
 reboot
